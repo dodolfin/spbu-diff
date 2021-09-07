3edcd6325c065e34d8e0db1877598d6a0ee36329
Denis Shabanov <shadenis2015@gmail.com>
2021-09-07 23:06:14 +0400

    Add function terminateOnError

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
+import kotlin.system.exitProcess
+
+/*
+ * В случае ошибки вывести сообщение [exitMessage] и завершить программу с ненулевым кодом возврата.
+ */
+fun terminateOnError(exitMessage: String) {
+    println(exitMessage)
+    exitProcess(1)
+}
+
 fun main(args: Array<String>) {
     TODO()
 }
