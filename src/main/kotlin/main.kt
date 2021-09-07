596d051c7b7d5b6137e1eca625d8351e176263b5
Denis Shabanov <shadenis2015@gmail.com>
2021-09-07 23:07:30 +0400

    Add naive implementation of readFromFile function

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,6 @@
 import kotlin.system.exitProcess
+import java.io.File
+import java.io.InputStream
 
 /*
  * В случае ошибки вывести сообщение [exitMessage] и завершить программу с ненулевым кодом возврата.
@@ -6,6 +8,18 @@ import kotlin.system.exitProcess
 fun terminateOnError(exitMessage: String) {
     println(exitMessage)
     exitProcess(1)
+}
+
+/*
+ * Считать содержимое файла, находящегося по адресу [pathToFile]. Пока что эта функция производит и проверки,
+ * связанные с файлом (существование, право доступа к нему и т. д.)
+ */
+fun readFromFile(pathToFile: String): Array<String> {
+    val inputStream: InputStream = File(pathToFile).inputStream()
+    val lineList = mutableListOf<String>()
+
+    inputStream.bufferedReader().forEachLine { lineList.add(it) }
+    return lineList.toTypedArray()
 }
 
 fun main(args: Array<String>) {
