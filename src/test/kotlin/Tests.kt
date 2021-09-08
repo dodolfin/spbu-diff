3898d88b7b0f85eb7c06f8eac0af162a0659531a
Denis Shabanov <shadenis2015@gmail.com>
2021-09-08 22:25:23 +0400

    Add tests for function compareTwoFiles

--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -1,9 +1,28 @@
 import kotlin.test.*
 
 internal class Tests {
-
     @Test
-    fun test1() {
-        assert(true)
+    fun compareTwoFilesTests() {
+        val file1Values = arrayOf(
+            arrayOf("a", "b", "c", "d", "e", "f", "g", "h"),
+            arrayOf("a", "b", "c", "d"),
+            arrayOf(),
+            arrayOf("b", "d", "p", "q", "v", "y", "z"),
+            arrayOf("#include <iostream>", "using namespace std;", "", "int main() {", "    int a, b;",
+                "    cin >> a >> b;", "    cout << a + b << endl;", "    return 0;", "}")
+        )
+        val file2Values = arrayOf(
+            arrayOf("b", "c", "e", "g", "h"),
+            arrayOf("e", "f", "g"),
+            arrayOf("a", "b"),
+            arrayOf("a", "q", "v", "b", "y", "d", "z"),
+            arrayOf("#include <iostream>", "using namespace std;", "", "int main() {", "    int a, b, c;",
+                "    cin >> a >> b >> c;", "    cout << a + b + c << endl;", "    return 0;", "}")
+        )
+        val answers = arrayOf(5, 0, 0, 4, 6)
+
+        for (i in file1Values.indices) {
+            assertEquals(answers[i], compareTwoFiles(file1Values[i], file2Values[i])[0].count { elem -> elem })
+        }
     }
 }
