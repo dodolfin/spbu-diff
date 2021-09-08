a783633613173ad26e798136e1a45680f90b83af
Denis Shabanov <shadenis2015@gmail.com>
2021-09-08 18:11:22 +0400

    Add few checks related to file existence, type etc. to readFromFile

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,6 +2,8 @@ import kotlin.system.exitProcess
 import java.io.File
 import java.io.InputStream
 
+const val SIZE_LIMIT = 1024 * 1024 * 1024
+
 /*
  * В случае ошибки вывести сообщение [exitMessage] и завершить программу с ненулевым кодом возврата.
  */
@@ -15,10 +17,26 @@ fun terminateOnError(exitMessage: String) {
  * связанные с файлом (существование, право доступа к нему и т. д.)
  */
 fun readFromFile(pathToFile: String): Array<String> {
-    val inputStream: InputStream = File(pathToFile).inputStream()
+    val fileObject = File(pathToFile)
+
+    if (!fileObject.exists()) {
+        terminateOnError("$pathToFile does not exist.")
+    }
+    if (!fileObject.isFile()) {
+        terminateOnError("$pathToFile is not a normal file.")
+    }
+    if (!fileObject.canRead() ) {
+        terminateOnError("$pathToFile is not readable.")
+    }
+    if (fileObject.length() > SIZE_LIMIT) {
+        terminateOnError("$pathToFile exceeds size limit ($SIZE_LIMIT bytes).")
+    }
+
+    val inputStream: InputStream = fileObject.inputStream()
     val lineList = mutableListOf<String>()
 
     inputStream.bufferedReader().forEachLine { lineList.add(it) }
+
     return lineList.toTypedArray()
 }
 
