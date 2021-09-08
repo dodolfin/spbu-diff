9e27d1581a47c59e3a02b7a6823e9e4fe083be1f
Denis Shabanov <shadenis2015@gmail.com>
2021-09-08 22:30:34 +0400

    Add naive implementation of compareTwoFiles function

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -25,7 +25,7 @@ fun readFromFile(pathToFile: String): Array<String> {
     if (!fileObject.isFile()) {
         terminateOnError("$pathToFile is not a normal file.")
     }
-    if (!fileObject.canRead() ) {
+    if (!fileObject.canRead()) {
         terminateOnError("$pathToFile is not readable.")
     }
     if (fileObject.length() > SIZE_LIMIT) {
@@ -38,6 +38,58 @@ fun readFromFile(pathToFile: String): Array<String> {
     inputStream.bufferedReader().forEachLine { lineList.add(it) }
 
     return lineList.toTypedArray()
+}
+
+/*
+ * Сравнивает два файла [file1] и [file2], представленные в виде массива строк,
+ * и возвращает ответ в следующем формате: solution с двумя массивами со значениями true или false,
+ * которые показывают, входит ли данная строка в НОП или нет.
+ */
+fun compareTwoFiles(file1: Array<String>, file2: Array<String>): Array<Array<Boolean>> {
+    /* LCS означает Longest Common Subsequence — наибольшую общую подпоследовательность
+     * В массиве LCSMemoization хранятся значения НОП для всех возможных префиксов двух файлов,
+     * а в массиве LCSReconstruction хранятся данные для восстановления.
+     */
+    val LCSMemoization = Array(file1.size + 1) { Array(file2.size + 1) { 0 } }
+    val LCSReconstruction = Array(file1.size + 1) { Array(file2.size + 1) { 0 } }
+
+    /*
+     * В массиве LCSReconstruction значение
+     *  1 означает, что в точке [prefix1 + 1][prefix2 + 1] последние строки совпали и их выгодно было включить в НОП
+     * -1 означает, что значение НОП было лучше в точке [prefix1][prefix2 + 1]
+     * -2 означает, что значение НОП было лучше в точке [prefix1 + 1][prefix2]
+     */
+    for (prefix1 in file1.indices) {
+        for (prefix2 in file2.indices) {
+            if (file1[prefix1] == file2[prefix2]) {
+                LCSMemoization[prefix1 + 1][prefix2 + 1] = LCSMemoization[prefix1][prefix2] + 1
+                LCSReconstruction[prefix1 + 1][prefix2 + 1] = 1
+            } else if (LCSMemoization[prefix1][prefix2 + 1] > LCSMemoization[prefix1 + 1][prefix2]) {
+                LCSMemoization[prefix1 + 1][prefix2 + 1] = LCSMemoization[prefix1][prefix2 + 1]
+                LCSReconstruction[prefix1 + 1][prefix2 + 1] = -1
+            } else {
+                LCSMemoization[prefix1 + 1][prefix2 + 1] = LCSMemoization[prefix1 + 1][prefix2]
+                LCSReconstruction[prefix1 + 1][prefix2 + 1] = -2
+            }
+        }
+    }
+
+    // Восстановление ответа
+    val solution = arrayOf(Array(file1.size) { false }, Array(file2.size) { false })
+    var prefix1 = file1.size; var prefix2 = file2.size
+    while (prefix1 != 0 && prefix2 != 0) {
+        when (LCSReconstruction[prefix1][prefix2]) {
+            1 -> {
+                solution[0][prefix1 - 1] = true
+                solution[1][prefix2 - 1] = true
+                prefix1--; prefix2--
+            }
+            -1 -> prefix1--
+            -2 -> prefix2--
+        }
+    }
+
+    return solution
 }
 
 fun main(args: Array<String>) {
