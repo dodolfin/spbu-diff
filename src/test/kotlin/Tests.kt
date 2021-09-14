b29cb74426a7157d0c9f294c23e24224bddb2d7c
Denis Shabanov <shadenis2015@gmail.com>
2021-09-14 19:11:12 +0300

    Add tests for stringsToLines function & Fix other tests with new types

--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -17,6 +17,64 @@ internal class Tests {
     }
 
     @Test
+    fun stringsToLinesTests() {
+        val strings1 = arrayOf(
+            arrayOf("a", "b", "c", "d", "e", "f", "g", "h"),
+            arrayOf("a", "b", "c", "d"),
+            arrayOf(),
+            arrayOf("b", "d", "p", "q", "v", "y", "z"),
+            arrayOf(
+                "#include <iostream>", "using namespace std;", "", "int main() {", "    int a, b;",
+                "    cin >> a >> b;", "    cout << a + b << endl;", "    return 0;", "}"
+            )
+        )
+        val strings2 = arrayOf(
+            arrayOf("b", "c", "e", "g", "h"),
+            arrayOf("e", "f", "g"),
+            arrayOf("a", "b"),
+            arrayOf("a", "q", "v", "b", "y", "d", "z"),
+            arrayOf(
+                "#include <iostream>", "using namespace std;", "", "int main() {", "    int a, b, c;",
+                "    cin >> a >> b >> c;", "    cout << a + b + c << endl;", "    return 0;", "}"
+            )
+        )
+        val stringsDictionary = arrayOf(
+            arrayOf("a", "b", "c", "d", "e", "f", "g", "h"),
+            arrayOf("a", "b", "c", "d", "e", "f", "g"),
+            arrayOf("a", "b"),
+            arrayOf("b", "d", "p", "q", "v", "y", "z", "a"),
+            arrayOf("#include <iostream>", "using namespace std;", "", "int main() {", "    int a, b;",
+                "    cin >> a >> b;", "    cout << a + b << endl;", "    return 0;", "}", "    int a, b, c;",
+                "    cin >> a >> b >> c;", "    cout << a + b + c << endl;"
+            )
+        )
+        val file1 = listOf(
+            listOf(0, 1, 2, 3, 4, 5, 6, 7),
+            listOf(0, 1, 2, 3),
+            listOf(),
+            listOf(0, 1, 2, 3, 4, 5, 6),
+            listOf(0, 1, 2, 3, 4, 5, 6, 7, 8)
+        )
+        val file2 = listOf(
+            listOf(1, 2, 4, 6, 7),
+            listOf(4, 5, 6),
+            listOf(0, 1),
+            listOf(7, 3, 4, 0, 5, 1, 6),
+            listOf(0, 1, 2, 3, 9, 10, 11, 7, 8)
+        )
+
+        for (i in strings1.indices) {
+            val comparisonOutputData = stringsToLines(strings1[i], strings2[i])
+            stringsDictionary[i].forEach { print("$it ") }
+            println()
+            comparisonOutputData.stringsDictionary.forEach { print("$it ") }
+            assertTrue(stringsDictionary[i].contentEquals(comparisonOutputData.stringsDictionary))
+            assertEquals(file1[i], comparisonOutputData.comparisonData.file1.map {it.stringIndex})
+            assertEquals(file2[i], comparisonOutputData.comparisonData.file2.map {it.stringIndex})
+        }
+    }
+
+    @Test
     fun compareTwoFilesTests() {
         val file1Values = arrayOf(
             arrayOf("a", "b", "c", "d", "e", "f", "g", "h"),
@@ -41,7 +99,12 @@ internal class Tests {
         val answers = arrayOf(5, 0, 0, 4, 6)
 
         for (i in file1Values.indices) {
-            assertEquals(answers[i], compareTwoFiles(file1Values[i], file2Values[i])[0].count { elem -> elem })
+            val comparisonOutputData = stringsToLines(file1Values[i], file2Values[i])
+            markNotCommonLines(comparisonOutputData)
+            compareTwoFiles(comparisonOutputData.comparisonData)
+
+            val file1 = comparisonOutputData.comparisonData.file1
+            assertEquals(answers[i], file1.count { it.lineMarker == LineMarker.COMMON })
         }
     }
 
@@ -64,7 +127,11 @@ internal class Tests {
         )
 
         for (i in file1Values.indices) {
-            plainOutput(file1Values[i], file2Values[i], compareTwoFiles(file1Values[i], file2Values[i]))
+            val comparisonOutputData = stringsToLines(file1Values[i], file2Values[i])
+            markNotCommonLines(comparisonOutputData)
+            compareTwoFiles(comparisonOutputData.comparisonData)
+
+            plainOutput(comparisonOutputData)
             assertEquals(answers[i], stream.toString().lines())
             stream.reset()
         }
@@ -92,7 +159,11 @@ internal class Tests {
         )
 
         for (i in file1Values.indices) {
-            normalOutput(file1Values[i], file2Values[i], compareTwoFiles(file1Values[i], file2Values[i]))
+            val comparisonOutputData = stringsToLines(file1Values[i], file2Values[i])
+            markNotCommonLines(comparisonOutputData)
+            compareTwoFiles(comparisonOutputData.comparisonData)
+
+            normalOutput(comparisonOutputData)
             assertEquals(answers[i], stream.toString().lines())
             stream.reset()
         }
